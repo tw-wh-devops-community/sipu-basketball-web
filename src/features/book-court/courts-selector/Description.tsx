d2b15bd8e3123536cfd440928191ce3fbdda28d2
lrliang <392425595@qq.com>
2021-04-12 16:07:10 +0800

    [LLR] add courts-selector description

---
+++ src/features/book-court/courts-selector/Description.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Description: React.FC = () => (
+  <ul className="description">
+    <li>Step1 请选择合适的场地 A/B/C/D/E/F</li>
+    <li>Step2 请选择左半场或右半场、或全场</li>
+  </ul>
+);
+
+export default Description;
--- src/features/book-court/courts-selector/index.tsx
+++ src/features/book-court/courts-selector/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Header from './Header';
+import Description from './Description';
 
 const CourtSelector: React.FC = () => (
   <div className="courts-elector">
     <Header />
+    <Description />
     {/* <CourtsBoard/> */}
     {/* <Confirmbutton/> */}
   </div>
